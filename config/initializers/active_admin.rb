4dd00fe0ba702eb5e1b156f90066fe025e8d4654
Ivan Hulko <johnyjcat@gmail.com>
2023-06-08 23:13:48 +0300

    fix redirect to admin page

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -3,6 +3,7 @@
 ActiveAdmin.setup do |config|
   config.site_title = 'Test For Codica'
   config.authentication_method = :authenticate_admin_user!
+  config.skip_before_action :authenticate_user!
   config.current_user_method = :current_admin_user
   config.logout_link_path = :destroy_admin_user_session_path
   config.batch_actions = true
